a9955709d7b398cf67a3baabede1d6776960a382
paolofantuz <fantuzpaolo@gmail.com>
2020-01-02 14:54:41 +0100

    Fixed User names

--- rankings.R
+++ rankings.R
@@ -9,22 +9,24 @@ rankings
 
 # Clean dataset
 rankings <- rankings %>% 
-  rename(rank=Rank, university=University, score=Score, country=Country, flag=Flag, league=League) %>%  # Reset names
-  select(-flag) %>%  # Cut useless columns
-  slice(0:225) %>%  # Get only first 200 rows
-  mutate(country=factor(country), league=factor(league))  # Factorize
+  rename(rank=Rank, university=University, score=Score, country=Country) %>%  # Reset names
+  select(-c(Flag, League)) %>%  # Cut useless columns
+  slice(0:225) %>%  # Get only first 225 rows
+  mutate(country=factor(country))  # Factorize
 rankings
 
 
 # Read twitter screen names
-snames <- read_delim('data/screen_names.txt', delim=' ', col_names=c('twitter'), col_types=cols(twitter=col_character()))
+snames <- read_delim('data/screen_names.txt', delim=' ', col_names=c('twitter_name'), col_types=cols(twitter_name=col_character()))
 snames
 
 # Add twitter screen names to rankings table
 rankings <- rankings %>%
-  add_column(twitter = snames$twitter)
+  add_column(twitter_name = snames$twitter_name)
 rankings
 
+# Filling missing Univerisities
+rankings[rankings$twitter_name == "None",]$twitter_name = NA
 
 # Save data to disk
 rankings %>% write_csv('data/rankings_clean.csv')
